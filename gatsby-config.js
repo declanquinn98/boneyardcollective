238d8a58eb1d51c41a3c89ddf495b54c2402ce26
Declan <dquinn133131@gmail.com>
2022-04-11 20:39:37 +1000

    csp 4?

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,26 +1,32 @@
 module.exports = {
-  siteMetadata: {
-    siteUrl: "http://127.0.0.1",
-    title: "The Boneyard Collective",
-  },
-  plugins: [
-    "gatsby-plugin-image",
-    "gatsby-plugin-react-helmet",
-    {
-      resolve: "gatsby-plugin-manifest",
-      options: {
-        icon: "src/images/icon3.png",
-      },
+    siteMetadata: {
+        siteUrl: "http://127.0.0.1",
+        title: "The Boneyard Collective",
     },
-    "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "images",
-        path: "src/images/",
-      },
-      __key: "images",
-    },
-  ],
+    plugins: [
+        "gatsby-plugin-image",
+        "gatsby-plugin-react-helmet",
+        {
+            resolve: "gatsby-plugin-manifest",
+            options: {
+                icon: "src/images/icon3.png",
+            },
+        },
+        "gatsby-plugin-sharp",
+        "gatsby-transformer-sharp",
+        {
+            resolve: "gatsby-source-filesystem",
+            options: {
+                name: "images",
+                path: "src/images/",
+            },
+            __key: "images",
+        },
+    ],
+    developMiddleware: app => {
+        app.use((req, res, next) => {
+            res.set('X-Frame-Options', 'https://declanquinn.dev');
+            next();
+        });
+    }
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -124,7 +124,7 @@ const IndexPage = () => {
                 />
                 <meta
                     http-equiv="Content-Security-Policy"
-                    content="X-Frame-Options https://declanquinn.dev; script-src 'self' declanquinn.dev 'unsafe-inline'; style-src 'self' 'unsafe-inline'; base-uri 'self'; frame-ancestors 'self' declanquinn.dev"
+                    content="X-Frame-Options https://declanquinn.dev; script-src 'self' declanquinn.dev 'unsafe-inline'; style-src 'self' 'unsafe-inline'; base-uri 'self'; frame-ancestors 'self' https://declanquinn.dev"
                 />
 
                 <title>The Boneyard Collective</title>
